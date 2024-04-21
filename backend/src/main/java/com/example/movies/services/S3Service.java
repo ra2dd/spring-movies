a2886b3b3bc5ff2a2faa2d7bca2b669a948fc078
ra2dd <radoslawpryw@gmail.com>
2024-04-21 15:33:54 +0200

    fix file uploading to Aws in S3Service

--- backend/src/main/java/com/example/movies/services/S3Service.java
+++ backend/src/main/java/com/example/movies/services/S3Service.java
@@ -1,23 +1,23 @@
 package com.example.movies.services;
 
 import io.awspring.cloud.s3.ObjectMetadata;
+import io.awspring.cloud.s3.S3Resource;
 import io.awspring.cloud.s3.S3Template;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.io.IOException;
-import java.time.Duration;
-import java.time.LocalDate;
 import java.util.UUID;
 
+@Service
 public class S3Service implements FileService {
 
     public static final String BUCKET_NAME = "movies-s3bucket";
-    private final S3Template s3Template;
-    private static final LocalDate startDate = LocalDate.of(2024, 1, 1);
-    private static final LocalDate endDate = LocalDate.of(2024, 12, 31);
+
+    S3Template s3Template;
 
     public S3Service(S3Template s3Template) {
         this.s3Template = s3Template;
@@ -28,29 +28,29 @@ public class S3Service implements FileService {
         // Prepare a Key
         var fileExtension = StringUtils.getFilenameExtension(file.getOriginalFilename());
 
-        var key = UUID.randomUUID().toString() + " " + fileExtension;
+        var key = UUID.randomUUID().toString() + "." + fileExtension;
 
         var metadata = new ObjectMetadata.Builder()
                 .contentType(file.getContentType())
                 .contentLength(file.getSize())
                 .build();
 
+        String videoUrl;
+
+        // Upload a file
         try {
-            s3Template.upload(
+            S3Resource s3Resource = s3Template.upload(
                     BUCKET_NAME,
                     key,
-                    file.getInputStream(),
-                    metadata
+                    file.getInputStream()
                     );
+            videoUrl = s3Resource.getURL().toString();
+
         } catch (IOException ioException) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,
                     "An error occurred while uploading a file.");
         }
 
-        return s3Template.createSignedGetURL(
-                BUCKET_NAME,
-                key,
-                Duration.between(startDate, endDate)
-        ).toString();
+        return videoUrl;
     }
 }
