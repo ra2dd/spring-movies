3ce35704b72573ad698713216f027a3765f2218c
ra2dd <radoslawpryw@gmail.com>
2024-05-30 10:17:14 +0200

    MOV-7 add subscribed videos component

--- frontend/movies-ui/src/app/app.routes.ts
+++ frontend/movies-ui/src/app/app.routes.ts
@@ -1,15 +1,20 @@
 import { Routes } from '@angular/router';
 import { UploadVideoComponent } from './upload-video/upload-video.component';
-import { AppComponent } from './app.component';
 import { SaveVideoDetailsComponent } from './save-video-details/save-video-details.component';
 import { VideoDetailComponent } from './video-detail/video-detail.component';
 import { HomeComponent } from './home/home.component';
+import { SubscribedVideosComponent } from './subscribed-videos/subscribed-videos.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
     title: 'Home',
+  },
+  {
+    path: 'subscribed',
+    component: SubscribedVideosComponent,
+    title: 'Subscribed'
   },
   {
     path: 'upload-video',
